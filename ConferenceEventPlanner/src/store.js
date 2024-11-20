ce2f12c13ce3bd4d400aff6ff71a34cf8a745c96
Shivam Khokhani <sxk2125@mavs.uta.edu>
2024-11-20 17:14:18 -0600

    React.js

---
+++ ConferenceEventPlanner/src/store.js
@@ -0,0 +1,13 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
+import venueReducer from './venueSlice';
+
+export default configureStore({
+  reducer: {
+    venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
+  },
+});
